0bda1bab966f8a9eb3ea0df9f0c57a6ed2b3174c
Ved <vedjet22@bergen.org>
2022-06-11 22:03:45 -0400

    split into 4 parts and boundaries

--- main.py
+++ main.py
@@ -13,22 +13,54 @@ while True:
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('frame', result)
-    cv2.imshow('mask', mask)
+    #cv2.imshow('frame', result)
+    #cv2.imshow('mask', mask)
 
+    (h, w) = frame.shape[:2]
+    
+    centerX, centerY = (w // 2), (h // 2)
 
-    count = 0
-    for i in range(10):
-        for j in range(10):
-            if (result[235+i][315+j] > [0, 0, 0]).all:
-                count+=1 
+    
+    topHalf = mask[centerY:h, 0:w]
+    bottomHalf = mask[0:centerY, 0:w]
+    leftHalf = mask[0:h, 0:centerX]
+    rightHalf = mask[0:h, centerX:w]
+    middleRadius = 50
+    middle = mask[centerY-middleRadius:centerY+middleRadius,centerX-middleRadius:centerX+middleRadius]
 
-            
+    success = (middleRadius*2)**2*255*0.3    
+
+    
+
+    a = np.sum(leftHalf)
+    b = np.sum(rightHalf)
+    c = np.sum(topHalf)
+    d = np.sum(bottomHalf)
+    e = np.sum(middle)
+    
+
+    #move left/move right
+
+    if e > success:
+        #stop
+        print("gaming time")
+    #if  a > b:
+        #move left
+        #print("left")
+    #elif b > a:
+        #move right
+        #print("right")
+
+    #cv2.imshow('Left Half', leftHalf)
+    #cv2.imshow('Right Half', rightHalf)  
+    #cv2.imshow('Top Half', topHalf)
+    #cv2.imshow('Bottom Half', bottomHalf)      
+    cv2.imshow('Middle', middle)
 
     if cv2.waitKey(1) == ord('q'):
         break
 
-print(count)
+
 
 cap.release()
 cv2.destroyAllWindows()
